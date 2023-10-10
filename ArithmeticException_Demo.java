ebfe113ab857718a5098b563db4de1dc756c4e88
chetanwakale <147504358+chetanwakale@users.noreply.github.com>
2023-10-10 16:40:00 +0530

    Add files via upload

---
+++ ArithmeticException_Demo.java
@@ -0,0 +1,16 @@
+
+class ArithmeticException_Demo 
+{ 
+	public static void main(String args[]) 
+	{ 
+		try { 
+			int a = 30, b = 0; 
+			int c = a/b; 
+			System.out.println ("Result = " + c); 
+		} 
+		catch(ArithmeticException e) { 
+			System.out.println ("Can't divide a number by 0"); 
+		} 
+	} 
+} 
+
